384e54350191bd536e64040250ea9830b9f59b29
Thejus <git@tigerraw22.anonaddy.com>
2022-05-29 14:55:24 +0530

    Design done a little more to go

--- main.py
+++ main.py
@@ -17,36 +17,120 @@ i = '''
       (__)(__)(_)\_)(____/   (___/ \___)(____)(___/(___/(_____)(_)\_)(___/\n
 
 '''
-computer_score = 0
-user_score = 0
+attempt = 1
 console = Console()
 list1 = [1, 2, 3]
 choices = ["Error", "Rock", "Paper", "Scissor"]
-result = pyfiglet.figlet_format(
-    "Rock Paper\n      and\n\t  Scissor", font="digital")
+my_data = []
 
-result = i
+
+won = ["You won this round"]
+loss = ["You loss this round"]
+tie = ["This round was a tie"]
+
+
+main_table = Table(show_header=True, header_style="bold magenta")
+main_table.add_column("Attempt No.", justify="center", style="dim", width=7)
+main_table.add_column("Bot choice", width=12)
+main_table.add_column("User choice", justify="right", width=12)
+main_table.add_column("Score", justify="center")
+
+computer_score = 0
+user_score = 0
+
+
 print("🪨📄✂")
 
 
 def check_win():
+    global computer_score,user_score
     if (choices[computer_choice] == choices[user_choice]):
-        print("Tie")
+        display_win(0)
     elif (computer_choice+1) % 3 == user_choice:
-        print("You Won")
+        user_score=1+user_score
+        display_win(1)
     else:
-        print("You Lose")
+        computer_score=1+computer_score
+        display_win(2)
 
 
-console.print("Welcome to \n",style="bold blue")
-console.print(result,style="bold blue")
-for _ in range(1, 4):
+def display_win(p):
+    global main_table
+    if p == 0:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Attempt No.", justify="center", width=7)
+        table.add_column("Bot choice", width=12)
+        table.add_column("User choice", justify="right", width=12)
+        table.add_column("Score", justify="center")
+        table.add_row(f"[bold yellow]{attempt}[/bold yellow]",
+                    f"[bold yellow]{choices[computer_choice]}[/bold yellow]", 
+                    f"[bold yellow]{choices[user_choice]}[/bold yellow]", 
+                    f"[bold yellow]{str(computer_score)}-{str(user_score)}[/bold yellow]")
+        console.print(table)
+        console.print(tabulate(my_data, headers=tie,
+                    tablefmt="grid"), style="bold yellow")
+        main_table.add_row(f"[bold yellow]{attempt}[/bold yellow]",
+                    f"[bold yellow]{choices[computer_choice]}[/bold yellow]", 
+                    f"[bold yellow]{choices[user_choice]}[/bold yellow]", 
+                    f"[bold yellow]{str(computer_score)}-{str(user_score)}[/bold yellow]")
+    elif p == 1:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Attempt No.", justify="center", width=7)
+        table.add_column("Bot choice", width=12)
+        table.add_column("User choice", justify="right", width=12)
+        table.add_column("Score", justify="center")
+        table.add_row(f"[bold green]{attempt}[/bold green]",
+                    f"[bold green]{choices[computer_choice]}[/bold green]", 
+                    f"[bold green]{choices[user_choice]}[/bold green]", 
+                    f"[bold green]{str(computer_score)}-{str(user_score)}[/bold green]")
+        console.print(table)
+        console.print(tabulate(my_data, headers=won,
+                    tablefmt="grid"), style="bold green")
+        main_table.add_row(f"[bold green]{attempt}[/bold green]",
+                    f"[bold green]{choices[computer_choice]}[/bold green]", 
+                    f"[bold green]{choices[user_choice]}[/bold green]", 
+                    f"[bold green]{str(computer_score)}-{str(user_score)}[/bold green]")
+    elif p == 2:
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Attempt No.", justify="center", width=7)
+        table.add_column("Bot choice", width=12)
+        table.add_column("User choice", justify="right", width=12)
+        table.add_column("Score", justify="center")
+        table.add_row(f"[bold red]{attempt}[/bold red]",
+                    f"[bold red]{choices[computer_choice]}[/bold red]", 
+                    f"[bold red]{choices[user_choice]}[/bold red]", 
+                    f"[bold red]{str(computer_score)}-{str(user_score)}[/bold red]")
+        console.print(table)
+        console.print(tabulate(my_data, headers=loss,
+                    tablefmt="grid"), style="bold red")
+        main_table.add_row(f"[bold red]{attempt}[/bold red]",
+                    f"[bold red]{choices[computer_choice]}[/bold red]", 
+                    f"[bold red]{choices[user_choice]}[/bold red]", 
+                    f"[bold red]{str(computer_score)}-{str(user_score)}[/bold red]")
+    else:
+        print("ERROR!")
+
+
+
+
+
+# * main code block
+console.print("Welcome to \n", style="bold blue")
+console.print(i, style="bold blue")
+while attempt <= 3:
     print("Enter your choice : ")
     print("\t1.Rock \n\t2.Paper\n\t3.Scissors")
     computer_choice = random.choice(list1)
-    user_choice = int(input(">"))
+    user_choice = int(input("👉 "))
     check_win()
-    print("\nComputer choice : ", choices[computer_choice],
-        "\nUser choice     : ", choices[user_choice])
-
-
+    attempt += 1
+console.print(main_table)
+if computer_score < user_score:
+    console.print(tabulate(my_data, headers=["Congrats, you won!!"],
+                    tablefmt="grid"), style="bold green")
+elif computer_score > user_score:
+    console.print(tabulate(my_data, headers=["You loss better luck next time!"],
+                    tablefmt="grid"), style="bold yellow")
+elif computer_score == user_score:
+    console.print(tabulate(my_data, headers=["The match was a tie"],
+                    tablefmt="grid"), style="bold blue")
